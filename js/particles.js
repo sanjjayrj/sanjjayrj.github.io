f1b04abe161f669713d924862f5bef5ad4ad998f
Sanjay Raju <sanjjayrj@gmail.com>
2025-01-19 01:32:24 +0300

    #29

--- js/particles.js
+++ js/particles.js
@@ -37,7 +37,7 @@ window.addEventListener('scroll', function () {
 // Create particles based on text
 ctx.fillStyle = "rgba(173, 216, 230, 0.8)"; // Coral Particles
 ctx.font = "4vmin Old English Text MT";
-ctx.fillText("Welcome to my world...", 100, 100 + scrollOffset);
+ctx.fillText("Innovating with AI", 100, 100 + scrollOffset);
 const textCoordinates = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
 // Particle blueprint
